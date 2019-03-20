f2e559a6ae3f5f969331eaa6e41792d8b03350f9
yasmine <yasminemedhat97@gmail.com>
2019-03-20 15:54:44 +0200

    images segmented using kmeans clustering and computing f-measure and conditional entropy for validation

--- img_segmentation.py
+++ img_segmentation.py
@@ -7,14 +7,15 @@ Created on Sun Mar 17 13:29:04 2019
 
 import os
 import numpy as np
-from PIL import Image
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import metrics
 import matplotlib.pyplot as plt
 import matplotlib.image as img
 from scipy.io import loadmat
+from sklearn.cluster import KMeans
+import imageio
+from skimage import img_as_uint
+from validation import validate_clustering
 
-#read images and return them in a list each image is 381*481
+#read images and return two lists; images and their names, each image is 381*481
 def read_images(path):
     #list to hold images
     images = []
@@ -24,9 +25,9 @@ def read_images(path):
     for file in files:
         file = file + '.jpg'
         images.append(img.imread(os.path.join(path, file))) 
-    return images
+    return files, images
 
-#read he ground truth images for each image
+#read the ground truth images for each image
 def read_groundTruth(path):
     groundT = []
     #take only mat files
@@ -47,17 +48,108 @@ def read_groundTruth(path):
 #display each image with its ground truth images
 def display_img_with_groundT(images, gt, index):
     n = len(gt[index]) + 1
-    fig, ax = plt.subplots(1, n, figsize=(20,20))
+    fig, ax = plt.subplots(1, n)
     ax[0].imshow(images[index])
     for i in range(len(gt[index])):
         ax[i+1].imshow(gt[index][i])
     plt.show()
         
-    
-    
+#segmentation using kmeans and return the list of images segmented to different clusters
+def Kmeans(data, n_clusters):
+    clusters = []
+    i = 0
+    for image in data:
+        #unroll image and represent it as one row of pixels eac pixel has 3 dimensions RGB
+        img_unrolled = image.reshape(image.shape[0]*image.shape[1],image.shape[2])
+        kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(img_unrolled)
+        #append the labels after reshaping to the shape of the real image 321*481
+        #kmeans.labels_.reshape(image.shape[:-1]))
+        
+        clusters.append(kmeans.labels_.reshape(image.shape[:-1]))
+        i = i + 1
+        if i % 25 == 0:
+            print(i*2,"%" ,"of images clustered")
+    return clusters 
 
-images = read_images(path1)
-gt = read_groundTruth(path2)
-print(len(gt[0]))
-for i in range(10):
-    display_img_with_groundT(images, gt, i)
+#compute kmeans for several different number of clusters
+def diff_kmeans(data, n_clusters_list):
+    results = []
+    for k in n_clusters_list:
+        print("computing clusters for k = ", k)
+        results.append(Kmeans(data, k))
+    return results
+
+#save the results of the the kmeans clustering
+def write_results(path, results, img_names, k_list):
+    i = 0
+    for k in k_list:
+        print("saving results of kmeans k =", k)
+        dir = os.path.join(path, str(k))
+        #make a new directory for each different k results
+        if not os.path.exists(dir):
+            os.makedirs(dir);
+        j = 0;
+        for image in results[i]:
+            rimg = img_as_uint(image)
+            imageio.imwrite(os.path.join(dir, img_names[j] + '.png'), rimg)
+            j = j + 1;
+        i = i + 1
+    print("all result saved!")
+
+#read saved kmeans clustering results directly
+def read_kmeans_results(path):
+    files = [f[:-4] for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.endswith('png')]
+    files.sort(key=int)
+    results = []
+    for file in files:
+        file = file + '.png'
+        total_path = os.path.join(path,file)
+        img = imageio.imread(total_path)
+        results.append(img)
+    return results
+ 
+#convert saved results to int32 so it could be displayed
+def convert_results_to_int32(results):    
+    converted_list = []
+    conv_arr = []    
+    for r in results:
+        for arr in r:
+            conv_arr.append(arr.astype(np.int32))
+        converted_list.append(conv_arr)
+    return converted_list
+
+
+#display a selected image with the segmented image after performing kmeans clustering wit each k in k_list
+def display_img_with_segmented_img(images, filenames, clustered_imgs, index, k_list):
+    #get number of images to be displayed; real image plus segmented image for each k in kmeans
+    n = len(k_list)+1
+    fig, ax = plt.subplots(1, n)
+    fig.suptitle('image: ' + filenames[index])
+    ax[0].imshow(images[index]) 
+    for i in range(len(k_list)):
+        ax[i+1].set_title('K = ' + str(k_list[i]));
+        ax[i+1].imshow(clustered_imgs[i][index]);
+    plt.show()   
+
+
+def main():  
+    path1 = r"E:\Documents\Term 8\Pattern Recognition\assignments\proj2\images"   
+    path2 = r"E:\Documents\Term 8\Pattern Recognition\assignments\proj2\groundTruth"
+    img_names, images = read_images(path1)
+    gt = read_groundTruth(path2)
+    for i in range(10):
+        display_img_with_groundT(images, gt, i)
+    k_list = [3]
+    kmeans_results = diff_kmeans(images, k_list)
+    path3= r"E:\Documents\Term 8\Pattern Recognition\assignments\proj2\kmeans_results"
+    write_results(path3, kmeans_results, img_names, k_list)
+    display_img_with_segmented_img(images, img_names, kmeans_results, 0, k_list)
+    path4=r"E:\Documents\Term 8\Pattern Recognition\assignments\proj2\kmeans_results"
+    #to read saved results directly
+    saved_results = []
+    for k in k_list:
+        res = read_kmeans_results(os.path.join(path4,str(k)))
+        saved_results.append(res)
+    saved_results = convert_results_to_int32(saved_results)
+    display_img_with_segmented_img(images, img_names, saved_results, 0, k_list)
+    total_fscore, avg_fscore, total_cEntropy, avg_cEntropy = validate_clustering(gt, saved_results, k_list)
---
+++ validation.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 20 14:45:40 2019
+
+@author: lenovo
+"""
+
+from sklearn.metrics.cluster import contingency_matrix
+import numpy as np
+import math
+
+#return the fmeasure of ground truth and kmeans-clustered image
+def f_measure(contigency_matrix):
+    fscore = 0
+    for i in range(len(contigency_matrix[0])):
+        maxi = np.argmax(contigency_matrix[:,i])
+        ni = np.sum(contigency_matrix[:,i])
+        prec = contigency_matrix[maxi][i] / ni
+        recall = contigency_matrix[maxi][i] / np.sum(contigency_matrix[maxi,:])
+        fscorei = (2 * prec * recall) / (prec + recall)
+        fscore += fscorei
+    return fscore/len(contigency_matrix[0])
+
+def conditional_entropy(contigency_matrix, n):
+    cEntropy = 0 
+    for i in range(len(contigency_matrix[0])):
+         entropy = 0
+         ni = np.sum(contigency_matrix[:,i])
+         for t in range(len(contigency_matrix)):
+             probT = contigency_matrix[t][i] / ni
+             if (probT != 0.0):
+                 entropy -= probT * math.log2(probT)
+         entropy *=  ni / n
+         cEntropy += entropy
+    return cEntropy
+    
+
+def validate_clustering(groundTruths, results, k_list):
+    #fmeasure and cond. entropy for all k parameters for all images, each image with M ground truths 
+    total_fscore = []
+    total_cEntropy = []
+    #average of M fmeasure and cond. entropy for all k parameters for all images truths
+    tot_avg_fscore = []
+    tot_avg_cEntropy = []
+    for k in range(len(k_list)):
+        fscore = []
+        cond_entr = []
+        avg_fscore = []
+        avg_cEnt = []
+        #for each segmented image
+        for result_img in results[k]:
+            f = []
+            cent = []
+            img = result_img.reshape(-1)
+            i = 0
+            #get fscore and entropy for each gt available for this image
+            for gt in groundTruths[i]:
+                gt = gt.reshape(-1)
+                #get the contingency matrix of the gt and the segmented image
+                contingency_mat= contingency_matrix(gt, img)
+                f.append(f_measure(contingency_mat))
+                cent.append(conditional_entropy(contingency_mat, len(gt)))
+            fscore.append(f);
+            cond_entr.append(cent);
+            avg_fscore.append(np.mean(f))
+            avg_cEnt.append(np.mean(cent))
+            i = i+ 1
+        total_fscore.append(fscore);
+        total_cEntropy.append(cond_entr)
+        tot_avg_fscore.append(avg_fscore)
+        tot_avg_cEntropy.append(avg_cEnt)
+    return total_fscore,tot_avg_fscore, total_cEntropy, tot_avg_cEntropy
+
+
+
